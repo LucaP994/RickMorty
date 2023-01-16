41829fa234f9edec86689884d1cf56fccb63b8d8
DESKTOP-NROK2GT\Luca <luca-puglisi@outlook.com>
2023-01-16 13:21:21 +0100

    add rotation and vertical padding

--- script/script.js
+++ script/script.js
@@ -2,109 +2,95 @@ let indexArr = []
 let page = 1;
 let pages = 0;
 let url = "https://rickandmortyapi.com/api/character?page="
-let credit = 50;
+let nextUrl = "";
+let credit = 300;
+let deckCards = 0;
 let cr = document.querySelector('#cr');
-let cards = [];
-let selectedCard;
-cr.innerText = cr.innerText+" "+credit;
-
+let d = document.querySelector('#d');
+let totalCards = [];
+let deck = [];
+let mano = [];
+cr.innerText = cr.innerText + " " + credit;
+d.innerText = d.innerText + " " + deckCards;
 fetch(url + page)
-.then(response => response.json())
-.then(res => {
-    page += 1;
-    let results = new SearchResult();
-    results = res;
-    pages = results.info.pages;
-    results.results.forEach(e => cards.push(e))
-    while(page < pages){
-        fetch(results.info.next)
-        .then(results => results.json())
-        .then(res => {
-            res.results.forEach(e => cards.push(e))
-        })
-    }
-});
-function pesca() {
-    if(credit <5){
+    .then(response => response.json())
+    .then(res => {
+        //page += 1;
+        let results;
+        results = res;
+        pages = results.info.pages;
+        nextUrl = results.info.next;
+        results.results.forEach(e => totalCards
+            .push(e));
+        while (page < pages) {
+            page += 1;
+            fetch(url + page)
+                .then(results => results.json())
+                .then(res => {
+                    nextUrl = res.info.next;
+                    console.log(nextUrl)
+                    res.results.forEach(e => totalCards
+                        .push(e))
+                })
+        }
+    });
+
+function acquista() {
+    if (credit < 5) {
         alert("crediti insufficienti");
-    }else{
-        credit -=5;
-        cr.innerText = "Crediti disponibili: "+credit;
-    console.log(page);
-    fetch(url + page)
-        .then(response => response.json())
-        .then(res => {
-            //console.log(res)
-            let container = document.querySelector('.container');
-            let arr = res.results;
-            let i = Math.floor(Math.random() * arr.length);
-
-            selectedCard = new CardModel();
-
-            if (!indexArr.includes(i)) {
-                selectedCard = arr[i];
-                indexArr.push(i);
-            } else {
-                console.log("indice già presente ");
-                if (indexArr.length != arr.length) {
-                    while (indexArr.includes(i)) {
-                        i = Math.floor(Math.random() * arr.length);
-                    }
-                    /*let i = Math.floor(Math.random() * arr.length);
-                    if(indexArr.includes(i)){
-                        i = Math.floor(Math.random() * arr.length);
-                        return i
-                    }*/
-                    selectedCard = arr[i];
-                    indexArr.push(i);
-                } else {
-                    indexArr = [];
-                    page++;
-                    console.log(indexArr);
-                }
-            }
-            console.log(indexArr);
-            console.log(selectedCard);
-            let card = document.createElement('div');
-            let img = document.createElement('img');
-            let boxInfo = document.createElement('div');
-            let name = document.createElement('h2');
-            let gender = document.createElement('p');
-            let species = document.createElement('p');
-            card.classList.add('card');
-            if (indexArr.length > 1) {
-                card.style.marginLeft = "-150px";
-            }
-            img.classList.add('img');
-            boxInfo.classList.add('boxInfo');
-            img.src = selectedCard.image;
-            name.innerText = selectedCard.name;
-            species.innerText = "Species: " + selectedCard.species;
-            gender.innerText = "Gender: " + selectedCard.gender;
-            boxInfo.append(name, species, gender);
-            card.appendChild(img);
-            card.appendChild(boxInfo);
-            container.appendChild(card);
-
-            /*arr.forEach(function (e) {
-                let card = document.createElement('div')
-                let img = document.createElement('img')
-                let boxInfo = document.createElement('div')
-                let name = document.createElement('h2')
-                let gender = document.createElement('p')
-                let species = document.createElement('p')
-                card.classList.add('card')
-                img.classList.add('img')
-                boxInfo.classList.add('boxInfo')
-                img.src = e.image
-                name.innerText = e.name
-                species.innerText = "Species: " + e.species
-                gender.innerText = "Gender: " + e.gender
-                boxInfo.append(name,species,gender)
-                card.appendChild(img)
-                card.appendChild(boxInfo)
-                container.appendChild(card)
-            })*/
-        });
+    } else {
+        credit -= 5;
+        deckCards += 1;
+        cr.innerText = "Crediti disponibili: " + credit;
+        d.innerText = "Carte nel deck: " + deckCards;
+        let i = Math.floor(Math.random() * totalCards
+            .length);
+        deck.push(totalCards[i]);
     }
-}
+};
+
+function pesca(){
+    let bodyRect = document.body.getBoundingClientRect();
+    if(deck.length > 0 && mano.length <= 5){
+        deckCards -= 1;
+        d.innerText = "Carte nel deck: " + deckCards;
+        let container = document.querySelector('.container');
+        let rand = Math.floor(Math.random() * deck.length)
+        let selectedCard = deck[rand];
+        console.log(selectedCard)
+        mano.push(selectedCard);
+        let card = document.createElement('div');
+        let img = document.createElement('img');
+        let boxInfo = document.createElement('div');
+        let name = document.createElement('h2');
+        let gender = document.createElement('p');
+        let species = document.createElement('p');
+        card.classList.add('card');
+        card.id = "c" + selectedCard.id;
+        if (mano.length > 1) {
+            card.style.marginLeft = "-150px";
+        }
+        img.classList.add('img');
+        boxInfo.classList.add('boxInfo');
+        img.src = selectedCard.image;
+        name.innerText = selectedCard.name;
+        species.innerText = "Species: " + selectedCard.species;
+        gender.innerText = "Gender: " + selectedCard.gender;
+        boxInfo.append(name, species, gender);
+        card.appendChild(img);
+        card.appendChild(boxInfo);
+        container.appendChild(card);
+        mano.forEach(c => {
+            let tCard = document.querySelector(`#c${c.id.toString()}`);
+            elemRect = tCard.getBoundingClientRect();
+            offset = (elemRect.left + ( elemRect.width / 2)) - bodyRect.left;
+            let centerCardOffset = offset - ( bodyRect.width /2 );
+            let angle = centerCardOffset / 10;
+            let top = 200 + (Math.abs(centerCardOffset) * 0.25);
+            console.log(top)
+            tCard.style.rotate = angle.toString() + "deg";     
+            tCard.style.top = top + "px";
+        })
+        deck = deck.filter(e => e.id != selectedCard.id);
+    }
+};
